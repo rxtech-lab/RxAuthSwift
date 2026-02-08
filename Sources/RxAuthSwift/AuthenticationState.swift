62b8cbbbbd93c58c5973a37ce2f03b015c8ed312
sirily11 <32106111+sirily11@users.noreply.github.com>
2026-02-08 17:21:18 +0800

    fix: user issue (#4)

--- Sources/RxAuthSwift/AuthenticationState.swift
+++ Sources/RxAuthSwift/AuthenticationState.swift
@@ -18,4 +18,44 @@ public struct User: Codable, Identifiable, Sendable, Equatable {
         self.email = email
         self.image = image
     }
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case sub
+        case name
+        case email
+        case image
+        case picture
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        // Support both "id" and "sub" (OIDC standard) for user identifier
+        if let id = try container.decodeIfPresent(String.self, forKey: .id) {
+            self.id = id
+        } else if let sub = try container.decodeIfPresent(String.self, forKey: .sub) {
+            self.id = sub
+        } else {
+            throw DecodingError.keyNotFound(
+                CodingKeys.id,
+                DecodingError.Context(
+                    codingPath: container.codingPath,
+                    debugDescription: "Neither 'id' nor 'sub' found in user info response"
+                )
+            )
+        }
+        self.name = try container.decodeIfPresent(String.self, forKey: .name)
+        self.email = try container.decodeIfPresent(String.self, forKey: .email)
+        // Support both "image" and "picture" (OIDC standard) for profile image
+        self.image = try container.decodeIfPresent(String.self, forKey: .image)
+            ?? container.decodeIfPresent(String.self, forKey: .picture)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encodeIfPresent(name, forKey: .name)
+        try container.encodeIfPresent(email, forKey: .email)
+        try container.encodeIfPresent(image, forKey: .image)
+    }
 }
